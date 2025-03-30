a53f4a4a18f6961754bdb6797eafc7fbe761e91e
David Flek <davidflek@backtech.cz>
2025-03-30 17:36:47 +0200

    chore: place-object demo

--- david-demo/main.js
+++ david-demo/main.js
@@ -1,6 +1,7 @@
 import "../primitives/ar-button.js";
 import "../components/ar/place-object.js";
 import "../components/ar/place-object-manager.js";
+import "../primitives/ar-menu.js";
 import 'aframe-extras';
 
 document.querySelector("#app").innerHTML = `
@@ -10,29 +11,37 @@ document.querySelector("#app").innerHTML = `
     webxr="optionalFeatures: hit-test;"
     ar-hit-test="mapSize: 0 0"
     touch-raycaster
-    place-object-manager
+    place-object-manager="
+        showPreview: true;
+    "
     context-menu
     cross-objects
 >
-    
-  <!-- Asset definitions -->
-  <a-assets>
-    <a-asset-item id="shrek" src="models/shrek_dancing.glb"></a-asset-item>
-  </a-assets>
   
   <!-- Placeable objects -->
-  <a-entity
-    id="chair-template"
-    gltf-model="#shrek"
+<a-ar-menu
+    position="0 1.5 -3"
     visible="false"
-    animation-mixer="clip: *;"
+    primary="#018A6C"
+    items="[
+            {'color':'white','icon':'/content-save','title':'Save','textColor':'black'},
+            {'color':'white','icon':'/close-circle','title':'Quit','textColor':'black'},
+            {'color':'white','icon':'/settings','title':'Settings','textColor':'black'},
+            {'color':'white','icon':'/file-plus','title':'New file','textColor':'black'}
+        ]"
+    variant="filled"
+    layout="circle"
     place-object="
       heightRange: 0 5;
-      surfaceTypes: horizontal;
-      distanceRange: 0.5 3.0;
-      scale: 0.2"
-  ></a-entity>
+      surfaceTypes: horizontal, ceiling, wall;
+      distanceRange: 0.5 10;
+      scale: 0.2;
+      isPoster: true;
+    "
+></a-ar-menu>
   
+
+      
   <!-- UI controls -->
   <a-entity id="controls" position="0 0 -1">
     <!--  Clear ALL btn-->
