16eb537887b8206e5d3244ec657ba4a4812e5254
David Flek <davidflek@backtech.cz>
2025-04-19 15:46:57 +0200

    chore: test-shrek

--- david-demo/main.js
+++ david-demo/main.js
@@ -18,49 +18,25 @@ document.querySelector("#app").innerHTML = `
     context-menu
     cross-objects
 >
+
+  <!-- Asset definitions -->
+  <a-assets>
+    <a-asset-item id="shrek" src="models/shrek_dancing.glb"></a-asset-item>
+  </a-assets>
   
   <!-- Placeable objects -->
-<a-ar-menu
-    position="0 1.5 -3"
+  <a-entity
+    id="chair-template"
+    gltf-model="#shrek"
     visible="false"
-    primary="#018A6C"
-    items="[
-            {'color':'white','icon':'/content-save','title':'Save','textColor':'black'},
-            {'color':'white','icon':'/close-circle','title':'Quit','textColor':'black'},
-            {'color':'white','icon':'/settings','title':'Settings','textColor':'black'},
-            {'color':'white','icon':'/file-plus','title':'New file','textColor':'black'}
-        ]"
-    variant="filled"
-    layout="circle"
     place-object="
       heightRange: 0 5;
       surfaceTypes: horizontal, ceiling, wall;
       distanceRange: 0.5 10;
       scale: 0.2;
-      isPoster: true;
+      isPoster: false;
       faceCamera: true;
-      customRotation: 30, 0, 180;
     "
-></a-ar-menu>
-      
-  <!-- UI controls -->
-  <a-entity id="controls" position="0 0 -1">
-    <!--  Clear ALL btn-->
-    <a-ar-button
-        billboard
-        position="0 1 0"
-        id="menu__toggle-button"
-        primary="green"
-        scale="0.2 0.2 0.2"
-        size="medium"
-        content="Clear All"
-        uppercase="true"
-        rounded="true"
-        outlined="true"
-        textcolor="white"
-        variant=""
-        onclick="window.ARObjectManager.removeAllObjects()"
-      ></a-ar-button>
-  </a-entity>
+   ></a-entity>
 </a-scene>
 `;
