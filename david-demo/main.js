dd6d798152acee1bd69b63f7c8ce8d28e0fc37e1
David Flek <davidflek@backtech.cz>
2025-04-21 19:44:08 +0200

    chore: test file

--- david-demo/main.js
+++ david-demo/main.js
@@ -26,42 +26,42 @@ document.querySelector("#app").innerHTML = `
   </a-assets>
   
 <!--   Placeable objects -->
-  <a-entity
-    id="chair-template"
-    gltf-model="#shrek"
-    visible="false"
-    place-object="    
-      heightRange: 0 5;
-      surfaceTypes: horizontal, ceiling, wall;
-      distanceRange: 0.5 10;
-      scale: 0.2;
-      isPoster: false;
-      faceCamera: true;
-      adjustOrientation: false;
-    "
-   ></a-entity>
-   
-<!--   <a-ar-menu-->
-<!--      position="0 1.5 -3"-->
-<!--      primary="#018A6C"-->
-<!--      items="[-->
-<!--                {'color':'white','icon':'/content-save','title':'Save','textColor':'black'},-->
-<!--                {'color':'white','icon':'/close-circle','title':'Quit','textColor':'black'},-->
-<!--                {'color':'white','icon':'/settings','title':'Settings','textColor':'black'},-->
-<!--                {'color':'white','icon':'/file-plus','title':'New file','textColor':'black'}-->
-<!--            ]"-->
-<!--      variant="filled"-->
-<!--      layout="circle"-->
-<!--      visible="false"-->
-<!--      place-object="-->
-<!--          heightRange: 0 5;-->
-<!--          surfaceTypes: horizontal, ceiling, wall;-->
-<!--          distanceRange: 0.5 10;-->
-<!--          scale: 0.2;-->
-<!--          isPoster: true;-->
-<!--          faceCamera: true;-->
-<!--          adjustOrientation: true;-->
+<!--  <a-entity-->
+<!--    id="chair-template"-->
+<!--    gltf-model="#shrek"-->
+<!--    visible="false"-->
+<!--    place-object="    -->
+<!--      heightRange: 0 5;-->
+<!--      surfaceTypes: horizontal, ceiling, wall;-->
+<!--      distanceRange: 0.5 10;-->
+<!--      scale: 0.2;-->
+<!--      isPoster: false;-->
+<!--      faceCamera: true;-->
+<!--      adjustOrientation: true;-->
 <!--    "-->
-<!--&gt;</a-ar-menu>-->
+<!--   ></a-entity>-->
+   
+   <a-ar-menu
+      position="0 1.5 -3"
+      primary="#018A6C"
+      items="[
+                {'color':'white','icon':'/content-save','title':'Save','textColor':'black'},
+                {'color':'white','icon':'/close-circle','title':'Quit','textColor':'black'},
+                {'color':'white','icon':'/settings','title':'Settings','textColor':'black'},
+                {'color':'white','icon':'/file-plus','title':'New file','textColor':'black'}
+            ]"
+      variant="filled"
+      layout="circle"
+      visible="false"
+      place-object="
+          heightRange: 0 5;
+          surfaceTypes: horizontal, ceiling, wall;
+          distanceRange: 0.5 10;
+          scale: 0.2;
+          isPoster: true;
+          faceCamera: true;
+          adjustOrientation: true;
+    "
+></a-ar-menu>
 </a-scene>
 `;
