dd317a26d0236c8d2b38fc0f56a7ac9a7f1abf4f
David Flek <davidflek@backtech.cz>
2025-04-21 18:43:34 +0200

    feat: better test demo

--- david-demo/main.js
+++ david-demo/main.js
@@ -3,6 +3,7 @@ import "../components/ar/place-object.js";
 import "../components/ar/place-object-manager.js";
 import "../primitives/ar-menu.js";
 import 'aframe-extras';
+import "../primitives/ar-menu.js";
 
 document.querySelector("#app").innerHTML = `
 <a-scene     
@@ -24,20 +25,43 @@ document.querySelector("#app").innerHTML = `
     <a-asset-item id="shrek" src="models/shrek_dancing.glb"></a-asset-item>
   </a-assets>
   
-  <!-- Placeable objects -->
+<!--   Placeable objects -->
   <a-entity
     id="chair-template"
     gltf-model="#shrek"
     visible="false"
-    place-object="
+    place-object="    
       heightRange: 0 5;
       surfaceTypes: horizontal, ceiling, wall;
       distanceRange: 0.5 10;
       scale: 0.2;
       isPoster: false;
-      faceCamera: false;
+      faceCamera: true;
       adjustOrientation: false;
     "
    ></a-entity>
+   
+<!--   <a-ar-menu-->
+<!--      position="0 1.5 -3"-->
+<!--      primary="#018A6C"-->
+<!--      items="[-->
+<!--                {'color':'white','icon':'/content-save','title':'Save','textColor':'black'},-->
+<!--                {'color':'white','icon':'/close-circle','title':'Quit','textColor':'black'},-->
+<!--                {'color':'white','icon':'/settings','title':'Settings','textColor':'black'},-->
+<!--                {'color':'white','icon':'/file-plus','title':'New file','textColor':'black'}-->
+<!--            ]"-->
+<!--      variant="filled"-->
+<!--      layout="circle"-->
+<!--      visible="false"-->
+<!--      place-object="-->
+<!--          heightRange: 0 5;-->
+<!--          surfaceTypes: horizontal, ceiling, wall;-->
+<!--          distanceRange: 0.5 10;-->
+<!--          scale: 0.2;-->
+<!--          isPoster: true;-->
+<!--          faceCamera: true;-->
+<!--          adjustOrientation: true;-->
+<!--    "-->
+<!--&gt;</a-ar-menu>-->
 </a-scene>
 `;
