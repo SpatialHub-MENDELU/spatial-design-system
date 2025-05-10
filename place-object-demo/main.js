9cbe1552ca8be2f5c17ed8e57c548d599aadebe6
David Flek <davidflek@backtech.cz>
2025-05-10 17:56:31 +0200

    chre: zmeny

--- place-object-demo/main.js
+++ place-object-demo/main.js
@@ -13,25 +13,6 @@ document.querySelector("#app").innerHTML = `
     context-menu
     cross-objects
 >
-
-  <!-- Asset definitions -->
-<!--  <a-assets>-->
-<!--    <a-asset-item id="shrek" src="models/shrek_dancing.glb"></a-asset-item>-->
-<!--    <a-asset-item id="mike" src="models/mike.glb"></a-asset-item>-->
-<!--&lt;!&ndash;    https://sketchfab.com/3d-models/mike-wazowski-sulley-face-swap-46f4f6948e25444880fcdfb4477390da&ndash;&gt;-->
-<!--  </a-assets>-->
-<!--  -->
-<!--  <a-entity-->
-<!--    gltf-model="#mike"-->
-<!--    visible="false"-->
-<!--    place-object="    -->
-<!--      surfaceTypes: horizontal, wall;-->
-<!--      scale: 0.0005;-->
-<!--      adjustOrientation: false;-->
-<!--      customRotation: 0 0 90;-->
-<!--    "-->
-<!--   ></a-entity>-->
-
 <a-ar-button
     visible="false"
     content="Button"
