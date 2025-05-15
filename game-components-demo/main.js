0926450c285279d779854cf1c49333cb264122f0
Kristýna Klimešová <klimesova.kristyna1@seznam.cz>
2025-05-15 10:30:17 +0200

    Add walk and gameview component

--- game-components-demo/main.js
+++ game-components-demo/main.js
@@ -1,4 +1,6 @@
 import './style.css'
+import '../components/game/walk.js'
+import '../components/game/gameview.js'
 
 document.querySelector('#app').innerHTML = `
     <a-scene physics=" driver: ammo; debug: true; debugDrawMode: 1;" inspector="true">
@@ -17,12 +19,14 @@ document.querySelector('#app').innerHTML = `
             <a-entity  gltf-model="#lion" ammo-shape="type: hull" position="0 -1.7 0.8" scale="0.5 0.5 0.5" ></a-entity>
         </a-entity>
         
-        
-        
         <!-- Characters -->
+        <a-entity walk id="fox-character" ammo-body="type: dynamic; angularFactor: 0 0 0; mass: 20; activationState: disableDeactivation" position="0 1.8 0">
+            <a-entity gltf-model="#fox" ammo-shape="type: hull;" position="0 -1.3 0.2" scale="1 1 1" ></a-entity>
+        </a-entity>
 
         <!-- Camera -->
-        <a-entity camera look-controls wasd-controls position="0 8 20" ></a-entity>
+<!--        <a-entity camera look-controls position="0 4 20" ></a-entity>-->
+        <a-entity camera gameview="target: #fox-character" rotation="-30 0 0"></a-entity>
         
     </a-scene>
 `
