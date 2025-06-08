a51379e2c9a29fb2537f41128b60dcdb5f5669bf
David Flek <davidflek@backtech.cz>
2025-06-08 12:14:11 +0200

    chore: cleanup demos

--- demos/place-object-demo/main.js
+++ demos/place-object-demo/main.js
@@ -1,9 +1,9 @@
-import "../primitives/ar-button.js";
-import "../components/ar/place-object.js";
-import "../components/ar/place-object-manager.js";
-import "../primitives/ar-menu.js";
+import "../../primitives/ar-button.js";
+import "../../components/ar/place-object.js";
+import "../../components/ar/place-object-manager.js";
+import "../../primitives/ar-menu.js";
 import 'aframe-extras';
-import "../primitives/ar-menu.js";
+import "../../primitives/ar-menu.js";
 
 document.querySelector("#app").innerHTML = `
 <a-scene     
