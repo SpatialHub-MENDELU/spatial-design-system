b8dd4be1faced480e726064cd4273f67ffbc514c
Tereza Dohnalova <xdohna10@mendelu.cz>
2025-08-01 15:55:19 +0200

    feat: add support for system keyboard when available

--- tereza-demo/main.js
+++ tereza-demo/main.js
@@ -1,11 +1,15 @@
 import "aframe";
 
 import "../primitives/ar-button.js";
+import "../primitives/ar-textbox.js";
+import "../components/autoXr.js";
 
 document.querySelector("#app").innerHTML = `
-<a-scene>
+<a-scene auto-xr>
 
-    <a-entity id="rig" hands></a-entity>
+    <a-camera>
+    <a-cursor></a-cursor>
+    </a-camera>
 
-    <a-ar-button content="Click Me" hand-clickable position="0 1.5 -2"></a-ar-button>
+    <a-ar-textbox label="Name" placeholder="Enter your name" position="0 1.5 -2"></a-ar-textbox>
     </a-scene>`;
