b14945a22c3331d9bcb1b914857403a870ae5034
Tereza Dohnalova <xdohna10@mendelu.cz>
2025-08-01 14:35:16 +0200

    fix: remove all non existing imports

--- tereza-demo/main.js
+++ tereza-demo/main.js
@@ -1,26 +1,11 @@
 import "aframe";
 
-import "../primitives/ar-textbox.js";
-import "../components/autoVr.js";
-import "../components/autoXr.js";
-import "../components/controllers.js";
-import "../components/vrinteractive.js";
-import "../components/controllerAttach.js";
-import "../components/ar/hands.js";
 import "../primitives/ar-button.js";
-import "../components/ar/ar-hoverable.js";
-import "../components/ar/ar-clickable.js";
-import "../components/ar/ar-stretchable.js";
 
 document.querySelector("#app").innerHTML = `
-<a-scene auto-xr="sessionMode: vr; autoEnter: false;
-        buttonText: Start VR"
-        webxr="optionalFeatures: hand-tracking"
-        >
+<a-scene>
 
-    <a-entity id="rig"
-       hands="leftHandColor: #d35400; rightHandColor:rgb(28, 225, 44)">
-    </a-entity>
+    <a-entity id="rig" hands></a-entity>
 
-    <a-box stretchable position="0 1.5 -2" color="#03FCC6"></a-box>
-</a-scene>`;
+    <a-ar-button content="Click Me" hand-clickable position="0 1.5 -2"></a-ar-button>
+    </a-scene>`;
