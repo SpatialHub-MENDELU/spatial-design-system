313e0ca9db94d8c033b0dfcbcb88f4b7e38c5601
Tereza Dohnalova <xdohna10@mendelu.cz>
2025-09-24 17:03:37 +0200

    fix: change scene

--- tereza-demo/main.js
+++ tereza-demo/main.js
@@ -7,13 +7,13 @@ import "../components/finger-touch.js";
 import "../primitives/ar-button.js";
 import "../primitives/ar-textbox.js";
 import "../components/autoXr.js";
+import "../components/controllers.js";
 
 document.querySelector("#app").innerHTML = `
 <a-scene auto-xr>
 
-    <a-camera>
-    <a-cursor></a-cursor>
-    </a-camera>
+  <a-entity id="rig" controllers>
+    </a-entity>
 
-    <a-ar-textbox label="Name" placeholder="Enter your name" position="0 1.5 -2"></a-ar-textbox>
+    <a-ar-textbox label="Name" placeholder="Enter your name" position="0 1.5 -2" useSystemKeyboard></a-ar-textbox>
     </a-scene>`;
