35cf0d06ca16ac834851d6166189ca78bae93ab3
Kristýna Klimešová <klimesova.kristyna1@seznam.cz>
2026-02-03 10:42:37 +0100

    Refactor

--- components/game/gameview.js
+++ components/game/gameview.js
@@ -1,49 +1,56 @@
+import { isPositiveNumber } from "../../utils/gameUtils";
+
+
 AFRAME.registerComponent("gameview", {
     schema: {
         target: {type: "selector"},
         height: {type: "number", default: 1},
         distance: {type: "number", default: 1},
         tilt: {type: "number", default: -15},
-        type: {type: "string", default: "third-person-fixed"}, // "quarter-turn", "third-person-fixed", "third-person-follow"
+        type: {type: "string", default: "thirdPersonFixed"}, // "quarterTurn", "thirdPersonFixed", "thirdPersonFollow"
         rotationSpeed : {type: "number", default: 5} // only for quarter-turn
     },
 
     init() {
-        this.cameraTypes = {
-            THIRD_PERSON_FIXED: "third-person-fixed",
-            QUARTER_TURN: "quarter-turn",
-            FIRST_PERSON: "first-person"
-        }
+        // types of cameras
+        this.thirdPersonFixed = false
+        this.thirdPersonFollow = false
+        this.quarterTurn = false
 
+        this.wrongInput = false
+        this.checkInputs()
+        if(this.wrongInput) return
 
-        const targetObject = this.data.target?.object3D;
-        if (!targetObject) {
-            console.error("Target is missing or invalid.");
-            return;
-        }
+        this.type = this.data.type
+        this.setType()
 
-        this.cameraType = this.data.type;
-        this.targetObject = targetObject;
+        if(this.thirdPersonFixed || this.quarterTurn || this.thirdPersonFollow) this.target = this.data.target?.object3D;
 
+        // thirdPersonFollow
         this.previousTargetPosition = new THREE.Vector3();
         this.angle = 0;
         this.targetAngle = 0;
+
+        // quarter turn
         this.rotationSpeed = this.data.rotationSpeed;
 
         this.bindKeyEvents();
         this.updateCamera();
     },
 
-    tick() {
-        if (!this.targetObject) return;
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // GENERAL METHODS
 
-        if (this.cameraType === this.cameraTypes.FIRST_PERSON) {
+    tick() {
+        if (!this.target) return;
+
+        if (this.thirdPersonFollow) {
             this.updateFirstPerson();
             return;
         }
 
         let diff = this.targetAngle - this.angle;
-        if (this.cameraType === this.cameraTypes.QUARTER_TURN) {
+        if (this.quarterTurn) {
             diff = ((diff + 540) % 360) - 180;
         }
 
@@ -56,9 +63,136 @@ AFRAME.registerComponent("gameview", {
         this.followTargetIfMoved();
     },
 
+    followTargetIfMoved() {
+        const currentPosition = this.target.position;
+        if (!this.previousTargetPosition.equals(currentPosition)) {
+            this.updateCamera();
+            this.previousTargetPosition.copy(currentPosition);
+        }
+    },
+
+    bindKeyEvents() {
+        document.addEventListener("keydown", (e) => {
+            if (!this.quarterTurn) return;
+            if (e.key === "e") this.targetAngle += 90;
+            if (e.key === "q") this.targetAngle -= 90;
+            this.targetAngle = (this.targetAngle + 360) % 360;
+            this.updateCamera();
+        })
+    },
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // CHECK INPUTS & UPDATE
+
+    checkInputs() {
+        if (!isPositiveNumber(this.data.height, 'height')) this.wrongInput = true
+        if (!isPositiveNumber(this.data.rotationSpeed, 'rotationSpeed')) this.wrongInput = true
+        if (!this.isValidCameraType(this.data.type)) this.wrongInput = true
+
+        if (!this.wrongInput) {
+            const needsTarget = ['thirdPersonFollow', 'thirdPersonFixed', 'quarterTurn'].includes(this.data.type);
+            if (needsTarget && !this.data.target?.object3D) {
+                console.error("Target is missing or invalid.");
+                this.wrongInput = true;
+            }
+        }
+    },
+
+    update(oldData) {
+        this.wrongInput = false
+        this.checkInputs()
+        if(this.wrongInput) return
+
+        if (oldData.type !== this.data.type) {
+            this.setType()
+            this.type = this.data.type;
+            this.updateCamera();
+        }
+
+        if (oldData.target !== this.data.target) {
+            this.target = this.data.target ? this.data.target.object3D : null;
+        }
+        if (oldData.height !== this.data.height) this.height = this.data.height
+        if (oldData.distance !== this.data.distance) this.distance = this.data.distance
+        if (oldData.tilt !== this.data.tilt) this.tilt = this.data.tilt
+        if (oldData.rotationSpeed !== this.data.rotationSpeed) this.rotationSpeed = this.data.rotationSpeed
+
+    },
+
+    isValidCameraType(type) {
+        const validTypes = ['thirdPersonFollow', 'thirdPersonFixed', 'quarterTurn'];
+        console.error(`Invalid camera type: ${type}. Valid types are: ${validTypes.join(', ')}.`);
+        return validTypes.includes(type);
+    },
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // SET METHODS
+
+    setType() {
+        this.thirdPersonFixed = false;
+        this.quarterTurn = false;
+        this.thirdPersonFollow = false;
+
+        switch(this.data.type) {
+            case 'thirdPersonFixed':
+                this.thirdPersonFixed = true
+                break;
+            case 'quarterTurn':
+                this.quarterTurn = true
+                break;
+            case 'thirdPersonFollow':
+                this.thirdPersonFollow = true
+                break;
+            default:
+                this.thirdPersonFixed = true
+                break
+        }
+    },
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // CAMERA TYPES METHODS
+
+    updateCamera() {
+        if(this.thirdPersonFollow) return;
+
+        const { x, y, z } = this.target.position;
+        const { height, distance, tilt } = this.data;
+
+        if (this.thirdPersonFixed) {
+            this.el.object3D.position.set(x, y + height, z + distance);
+            this.el.object3D.rotation.set(
+                THREE.MathUtils.degToRad(tilt),
+                0,
+                0
+            );
+        }
+
+        if (this.quarterTurn) {
+            const radiansY = THREE.MathUtils.degToRad(this.angle);
+            const radiansTilt = THREE.MathUtils.degToRad(tilt);
+
+            const offsetX = Math.sin(radiansY) * distance;
+            const offsetZ = Math.cos(radiansY) * distance;
+
+            this.el.object3D.position.set(
+                x + offsetX,
+                y + height,
+                z + offsetZ
+            );
+
+            this.el.object3D.rotation.set(
+                radiansTilt,
+                radiansY,
+                0
+            );
+        }
+    },
+
+    // third-person-follow
+
     updateFirstPerson() {
-        const { x, y, z } = this.targetObject.position;
-        const targetRotationY = this.targetObject.rotation.y;
+        const { x, y, z } = this.target.position;
+        const targetRotationY = this.target.rotation.y;
 
         const rotationFlippedY = targetRotationY + Math.PI;
 
@@ -80,64 +214,4 @@ AFRAME.registerComponent("gameview", {
         );
     },
 
-    updateCamera() {
-        if(this.cameraType === this.cameraTypes.FIRST_PERSON) return;
-
-        const { x, y, z } = this.targetObject.position;
-        const { height, distance, tilt } = this.data;
-
-        if (this.cameraType === this.cameraTypes.THIRD_PERSON_FIXED) {
-            this.el.object3D.position.set(x, y + height, z + distance);
-            this.el.object3D.rotation.set(
-                THREE.MathUtils.degToRad(tilt),
-                0,
-                0
-            );
-        }
-
-        if (this.cameraType === this.cameraTypes.QUARTER_TURN) {
-            const radiansY = THREE.MathUtils.degToRad(this.angle);
-            const radiansTilt = THREE.MathUtils.degToRad(tilt);
-
-            const offsetX = Math.sin(radiansY) * distance;
-            const offsetZ = Math.cos(radiansY) * distance;
-
-            this.el.object3D.position.set(
-                x + offsetX,
-                y + height,
-                z + offsetZ
-            );
-
-            this.el.object3D.rotation.set(
-                radiansTilt,
-                radiansY,
-                0
-            );
-        }
-    },
-
-    followTargetIfMoved() {
-        const currentPosition = this.targetObject.position;
-        if (!this.previousTargetPosition.equals(currentPosition)) {
-            this.updateCamera();
-            this.previousTargetPosition.copy(currentPosition);
-        }
-    },
-
-    bindKeyEvents() {
-        document.addEventListener("keydown", (e) => {
-            if (this.cameraType !== this.cameraTypes.QUARTER_TURN) return;
-            if (e.key === "e") this.targetAngle += 90;
-            if (e.key === "q") this.targetAngle -= 90;
-            this.targetAngle = (this.targetAngle + 360) % 360;
-            this.updateCamera();
-        })
-    },
-
-    update(oldData) {
-        if (oldData.type !== this.data.type) {
-            this.cameraType = this.data.type;
-            this.updateCamera();
-        }
-    }
 });
